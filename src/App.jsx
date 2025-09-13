1a465e4b3be4b6c5685b309f0dedead7bfadb6bb
prashan <prashanramachandran123@gmail.com>
2025-09-13 18:59:43 +0530

    setup the ground level set of the app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-white'>welcome to the Potfolio website!</h1>
+    </div>
+  )
+}
+
+export default App;
